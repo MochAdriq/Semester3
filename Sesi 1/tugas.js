bf5cab26c483f01dcf1971f2e2e819f1356ff403
MochAdriq <114837525+MochAdriq@users.noreply.github.com>
2023-09-27 12:27:20 +0700

    Create tugas.js

---
+++ Sesi 1/tugas.js
@@ -0,0 +1,14 @@
+const data = {
+    nama : "adriq",
+    NIM : 20220040047
+};
+
+const SKS = {
+    sem1 : 20,
+    sem2 : 21
+};
+
+console.log("Nama saya adalah : ", data.nama)
+console.log("NIM saya adalah : ", data.NIM)
+console.log("SKS yang saya tempuh selama 2 semester adalah : ", SKS.sem1 + SKS.sem2)
+console.log("SKS yang saya tempuh selama 2 semester adalah : ", SKS.sem1 + SKS.sem2)
