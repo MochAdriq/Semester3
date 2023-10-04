d16da317bc1555eff37c43b9156e675b2dd64f87
MochAdriq <114837525+MochAdriq@users.noreply.github.com>
2023-10-04 12:01:52 +0700

    Create TugasSesi2.js

---
+++ sesi 2/TugasSesi2.js
@@ -0,0 +1,8 @@
+let firstName = "Moch"
+let lastName = "Adriq"
+let age = 19
+let isMarried = false;
+
+console.log(`Nama saya ${firstName} ${lastName}`)
+console.log(`Umur saya ${age}`)
+console.log(`Status menikah saya adalah ${isMarried?"Sudah":"Belum"} menikah`)
